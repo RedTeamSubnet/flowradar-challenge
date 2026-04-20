09a61ec47384f1d48b7e48ba41751d6c415daba9
aliyuldashev <javohir.hbai@gmail.com>
2026-04-21 08:50:38 +0900

    refactor: Making it working )

--- src/flr_challenge/challenge/api/core/configs/_challenge.py
+++ src/flr_challenge/challenge/api/core/configs/_challenge.py
@@ -102,7 +102,7 @@ class ChallengeConfig(BaseConfig):
     @model_validator(mode="after")
     def _check_all(self) -> Self:
         DATA_DIR = os.getenv(
-            f"{ENV_PREFIX}CHALLENGE_API_DATA_DIR", "/var/lib/historical_fingerprinter/"
+            f"{ENV_PREFIX}CHALLENGE_API_DATA_DIR", "/var/lib/flowradar-challenge"
         )
         if not os.path.isdir(DATA_DIR):
             os.makedirs(DATA_DIR, exist_ok=True)
